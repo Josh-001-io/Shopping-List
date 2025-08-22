20694fd88495f469dd2fea06dad6172905a6be97
Josh-001-io <josiahochai@gmail.com>
2025-08-22 03:46:51 +0100

    updated code

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
-function addItem(e) {
-    e.preventDefault();
 
-    const  newItem = itemInput.value
+function onAddItemSubmit(e) {
+  e.preventDefault();
 
-    // Validate Input
-    if (newItem.value === '' ) {
-        alert('Please add an item');
-        return;
-    }
+  const newItem = itemInput.value;
 
-    // create List Item
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(newItem));
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
 
-    const button = createButton("remove-item btn-link text-red");
-    li.appendChild(button);
+  addItemToDOM(newItem);
 
-    itemList.appendChild(li);
-
-    itemInput.value = '';
 }
 
+function addItemToDOM(item) {
+
+      // create List Item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+
+  checkUI();
+
+  itemInput.value = "";
+}
 
 function createButton(classes) {
     const button = document.createElement('button');
@@ -40,5 +51,63 @@ function createIcon(classes) {
     return icon;
 }
 
+
+function removeItem(e) { 
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm('Are you sure')) {
+          e.target.parentElement.parentElement.remove();
+        }
+    }
+    checkUI();
+}
+
+
+function addItemToStorage(item) {
+    let itemFromStorage;
+}
+
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+
+}
+
+function filterItems(e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) => { 
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        if (itemName.includes(text)) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = "none";
+            
+        }
+    });
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll("li");
+
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+         clearBtn.style.display = "block";
+         itemFilter.style.display = "block";
+    }   
+}
+
+
 // Even Listeners
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
